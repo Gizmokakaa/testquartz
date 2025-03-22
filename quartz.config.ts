17eb932f39dc2aedff511480b0c065f61aa39cd3
Gizmokakaa <134132966+Gizmokakaa@users.noreply.github.com>
2025-03-22 22:06:00 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#FAF4ED",
+          light: "#faf4ed",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
